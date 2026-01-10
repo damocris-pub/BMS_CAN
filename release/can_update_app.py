7a3bd232058e195ec74b1f5df48226ee6ee20d16
Richard Xu <junzexu@outlook.com>
2026-01-10 10:23:32 +0800

    update python script

--- release/can_update_app.py
+++ release/can_update_app.py
@@ -6,14 +6,15 @@ from ctypes import *
 import threading
 import time
 import sys
+import struct 
 
 udll = cdll.LoadLibrary('./can_update.dll')
 
 #uint16_t crc16(uint8_t *buffer, uint32_t len, uint16_t start)
-udll.crc16.argtypes = [c_char_p, c_uint, c_ushort]
+udll.crc16.argtypes = [POINTER(c_ubyte), c_uint, c_ushort]
 udll.crc16.restype = c_ushort
 #uint32_t crc32(uint8_t *buffer, uint32_t len, uint32_t start)
-udll.crc32.argtypes = [c_char_p, c_uint, c_uint]
+udll.crc32.argtypes = [POINTER(c_ubyte), c_uint, c_uint]
 udll.crc32.restype = c_uint
 #bool can_connect(int zlg_chan, int can_speed)
 udll.can_connect.argtypes = [c_int, c_int]
@@ -57,7 +58,7 @@ udll.can_setPacketSeqCmd.restype = c_int
 udll.can_setPacketAddrCmd.argtypes = [c_ubyte, c_uint, c_char_p]
 udll.can_setPacketAddrCmd.restype = c_int
 #int can_sendPacketData(uint16_t packetLen, uint8_t *data)
-udll.can_sendPacketData.argtypes = [c_ushort, c_char_p]
+udll.can_sendPacketData.argtypes = [c_ushort, POINTER(c_ubyte)]
 udll.can_sendPacketData.restype = c_int
 #int can_verifyPacketDataCmd(uint8_t addr, uint16_t packetCrc)
 udll.can_verifyPacketDataCmd.argtypes = [c_ubyte, c_ushort]
@@ -79,7 +80,7 @@ kCanSpeed = 500000      #bps
 kBMSAddr = 0            #bms slave address, start from 0
 kMode = 0               #0 - current station, 1 - all stations
 kCrcType = 0            #0 - crc16, 1 - crc32
-kPacketLen = 128        #options: 8, 16, 32, 64, 128, 256, 512
+kPacketLen = 256        #options: 8, 16, 32, 64, 128, 256, 512
 kBmsFile = "bms.dfu"
 
 try:
@@ -92,7 +93,7 @@ fileLen = len(content)
 if ((fileLen & (kPacketLen - 1)) != 0):
     print("file length is not multiple of packetLength")
     sys.exit(1)
-buffer = (c_ubyte * fileLen).from_buffer_copy(content)
+base_ptr = cast((c_ubyte * fileLen).from_buffer_copy(content), POINTER(c_ubyte))
 
 print(f"target address is {kBMSAddr}, packet length is {kPacketLen}, mode is {kMode}, and crc type is {kCrcType}")
 success = udll.can_connect(c_int(kZlgCanChn), c_int(kCanSpeed))
@@ -113,13 +114,14 @@ if (success < 0):
     print("could not fetch LV BMS bootloader's version")
     print("Are you sure the board fw is in the dfu mode?");
     sys.exit(1)
-print(f"LV BMS FW bootloader's version is {resp[3]}.{resp[2]}.{resp[1]}, build is {resp[0]}, HW is {resp[4]}")
+print(f"LV BMS FW bootloader's version is {struct.unpack('B', resp[3])[0]}.{struct.unpack('B', resp[2])[0]}.{struct.unpack('B', resp[1])[0]}, build is {struct.unpack('B', resp[0])[0]}, HW is {struct.unpack('B', resp[4])[0]}")
 
 success = udll.can_getApplicationVerCmd(c_ubyte(kBMSAddr), resp)
 if (success < 0):
     print("could not fetch LV BMS application's version")
     sys.exit(1)
-print(f"LV BMS FW application's version is {resp[4]}.{resp[3]}.{resp[2]}, build is {(resp[1] << 8) | resp[0]}")
+var = (struct.unpack('B', resp[1])[0] << 8) | (struct.unpack('B', resp[0])[0])
+print(f"LV BMS FW application's version is {struct.unpack('B', resp[4])[0]}.{struct.unpack('B', resp[3])[0]}.{struct.unpack('B', resp[2])[0]}, build is {var}")
 
 success = udll.can_getHardwareTypeCmd(c_ubyte(kBMSAddr), resp)
 if (success < 0):
@@ -131,14 +133,18 @@ success = udll.can_getHardwareInfoCmd(c_ubyte(kBMSAddr), resp)
 if (success < 0):
     print("could not get hardware info")
     sys.exit(1)
-print(f"LV BMS's hardware build date is year:20{resp[0]:02}, month:{resp[1]:02}, day:{resp[2]:02}, batch no: {(resp[4] << 8) | resp[3]}")
+year = struct.unpack('B', resp[0])[0]
+month = struct.unpack('B', resp[1])[0]
+day = struct.unpack('B', resp[2])[0]
+var = (struct.unpack('B', resp[4])[0] << 8) | (struct.unpack('B', resp[3])[0])
+print(f"LV BMS's hardware build date is year:20{year:02}, month:{month:02}, day:{day:02}, batch no: {var}")
 
 batterySN = create_string_buffer(33)
 success = udll.can_getHardwareInfoCmd(c_ubyte(kBMSAddr), batterySN)
 if (success < 0):
     print("could not get battery sn")
     sys.exit(1)
-print(f"LV BMS's battery sn is {batterySN.value.decode('utf-8')}")
+#print(f"LV BMS's battery sn is {batterySN.value.decode('utf-8')}")
 
 success = udll.can_setPacketLenCmd(c_ubyte(kBMSAddr), c_ushort(kPacketLen))
 if (success < 0):
@@ -149,36 +155,40 @@ success = udll.can_getPacketLenCmd(c_ubyte(kBMSAddr), resp)
 if (success < 0):
     print("try to get packet length failed")
     sys.exit(1)
-if (((resp[1] << 8) | resp[0]) != kPacketLen):
+var = (struct.unpack('B', resp[1])[0] << 8) | (struct.unpack('B', resp[0])[0])
+if (var != kPacketLen):
     print("packetLen are not equal")
 
 success = udll.can_setApplicationLenCmd(c_ubyte(kBMSAddr), c_uint(fileLen), resp)
-if (success < 0 or ((resp[3] << 24) | (resp[2] << 16) | (resp[1] << 8) | resp[0]) != fileLen):
+var = (struct.unpack('B', resp[3])[0] << 24) | (struct.unpack('B', resp[2])[0] << 16) | (struct.unpack('B', resp[1])[0] << 8) | (struct.unpack('B', resp[0])[0])
+if (success < 0 or var != fileLen):
     print(f"try to set application length {fileLen} failed")
     sys.exit(1)
 
 seq = 1
 while (seq <= fileLen//kPacketLen):     #TODO: test with different sequence order
     success = udll.can_setPacketSeqCmd(c_ubyte(kBMSAddr), c_ushort(seq), resp)
-    if (success < 0 or ((resp[1] << 8) | resp[0]) != seq):
+    var = (struct.unpack('B', resp[1])[0] << 8) | (struct.unpack('B', resp[0])[0])
+    if (success < 0 or var != seq):
         print(f"try to set packet sequence num {seq} failed")
         sys.exit(1)
-    success = udll.can_sendPacketData(c_ushort(kPacketLen), buffer + (seq-1) * kPacketLen)
+    current_ptr = cast(c_void_p(addressof(base_ptr.contents) + (seq-1) * kPacketLen), POINTER(c_ubyte)) 
+    success = udll.can_sendPacketData(c_ushort(kPacketLen), current_ptr)
     if (success < 0):
         print(f"try to send packet data for {seq} failed")
         sys.exit(1)
-    packetCrc = udll.crc16(buffer + (seq-1) * kPacketLen, c_uint(kPacketLen), c_ushort(0xFFFF))
+    packetCrc = udll.crc16(current_ptr, c_uint(kPacketLen), c_ushort(0xFFFF))
     print(f"packet seq {seq} 's crc is 0x{packetCrc:04x}")
-    success = udll.can_verifyPacketDataCmd(c_ushort(kPacketLen), c_ushort(packetCrc))
+    success = udll.can_verifyPacketDataCmd(c_ubyte(kBMSAddr), c_ushort(packetCrc))
     if (success < 0):
         print(f"try to verify packet crc for {seq} failed")
         sys.exit(1)
     seq = seq + 1
 
 if (kCrcType == 0): #crc16
-    fileCrc = udll.crc16(buffer, c_uint(fileLen), c_ushort(0xFFFF))
+    fileCrc = udll.crc16(base_ptr, c_uint(fileLen), c_ushort(0xFFFF))
 else:   #crc32
-    fileCrc = udll.crc32(buffer, c_uint(fileLen), c_uint(0))
+    fileCrc = udll.crc32(base_ptr, c_uint(fileLen), c_uint(0))
 success = udll.can_verifyAllDataCmd(c_ubyte(kBMSAddr), c_ubyte(kCrcType), c_uint(fileCrc))
 if (success < 0):
     print("try to set verify application failed")
@@ -204,7 +214,7 @@ while (True):
     if (success < 0):
         print("try to get update status failed")
         sys.exit(1)
-    status = resp[0]
+    status = struct.unpack('B', resp[0])[0]
     if (status == 0xAA):
         print("update bms app successfully")
         break
